0089f0945e6f7d3dd7b7f659ed21f7ace5391bd8
Lammyy <mr.alexlam@yahoo.com>
2018-06-14 17:07:57 +1000

    Add files via upload

---
+++ mnistgan.py
@@ -0,0 +1,134 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+# Training Params
+num_steps = 100000
+batch_size = 128
+learning_rate = 0.0001
+
+# Network Params
+image_dim = 784 # 28*28 pixels
+gen_hidden_dim = 256
+disc_hidden_dim = 256 #number of nodes per layer
+noise_dim = 100 # Noise data points used to generate data
+#Xavier initialization determines the min and max initial weight values depending
+#on number of input and outputs neurons
+#if weights are too small then signal is too small and vice versa for large
+def xavier_init(fan_in, fan_out, constant=1):
+    low = -constant*np.sqrt(6.0/(fan_in + fan_out))
+    high = constant*np.sqrt(6.0/(fan_in + fan_out))
+    return tf.random_uniform((fan_in, fan_out),
+                             minval=low, maxval=high,
+                             dtype=tf.float32)
+
+# Store layers weight & bias
+weights = {
+    'gen_hidden1': tf.Variable(xavier_init(noise_dim, gen_hidden_dim)),
+    'gen_out': tf.Variable(xavier_init(gen_hidden_dim, image_dim)),
+    'disc_hidden1': tf.Variable(xavier_init(image_dim, disc_hidden_dim)),
+    'disc_out': tf.Variable(xavier_init(disc_hidden_dim, 1)),
+}
+biases = {
+    'gen_hidden1': tf.Variable(tf.zeros([gen_hidden_dim])),
+    'gen_out': tf.Variable(tf.zeros([image_dim])),
+    'disc_hidden1': tf.Variable(tf.zeros([disc_hidden_dim])),
+    'disc_out': tf.Variable(tf.zeros([1])),
+}
+
+
+# Generator takes in noise and outputs image
+def generator(x):
+    hidden_layer = tf.matmul(x, weights['gen_hidden1'])
+    hidden_layer = tf.add(hidden_layer, biases['gen_hidden1'])
+    hidden_layer = tf.nn.relu(hidden_layer)
+    out_layer = tf.matmul(hidden_layer, weights['gen_out'])
+    out_layer = tf.add(out_layer, biases['gen_out'])
+    out_layer = tf.nn.sigmoid(out_layer)
+    return out_layer
+
+
+# Discriminator takes in image and outputs classification (real/fake)
+def discriminator(x):
+    hidden_layer = tf.matmul(x, weights['disc_hidden1'])
+    hidden_layer = tf.add(hidden_layer, biases['disc_hidden1'])
+    hidden_layer = tf.nn.relu(hidden_layer)
+    out_layer = tf.matmul(hidden_layer, weights['disc_out'])
+    out_layer = tf.add(out_layer, biases['disc_out'])
+    out_layer = tf.nn.sigmoid(out_layer)
+    return out_layer
+
+# Build Networks
+# Network Inputs
+gen_input = tf.placeholder(tf.float32, shape=[None, noise_dim], name='input_noise')
+disc_input = tf.placeholder(tf.float32, shape=[None, image_dim], name='disc_input')
+
+# Build Generator Network
+gen_sample = generator(gen_input)
+
+# Build 2 Discriminator Networks (one from noise input, one from generated samples)
+disc_real = discriminator(disc_input)
+disc_fake = discriminator(gen_sample)
+
+# Build Loss
+gen_loss = -tf.reduce_mean(tf.log(disc_fake))
+disc_loss = -tf.reduce_mean(tf.log(disc_real) + tf.log(1. - disc_fake))
+
+# Build Optimizers
+optimizer_gen = tf.train.AdamOptimizer(learning_rate=learning_rate)
+optimizer_disc = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+# Generator Network Variables
+gen_vars = [weights['gen_hidden1'], weights['gen_out'],
+            biases['gen_hidden1'], biases['gen_out']]
+# Discriminator Network Variables
+disc_vars = [weights['disc_hidden1'], weights['disc_out'],
+            biases['disc_hidden1'], biases['disc_out']]
+
+# Create training operations
+train_gen = optimizer_gen.minimize(gen_loss, var_list=gen_vars)
+train_disc = optimizer_disc.minimize(disc_loss, var_list=disc_vars)
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+# Start training
+with tf.Session() as sess:
+
+    # Run the initializer
+    sess.run(init)
+
+    for i in range(1, num_steps+1):
+        # Prepare Data
+        # Get the next batch of MNIST data (only images are needed, not labels)
+        batch_x, _ = mnist.train.next_batch(batch_size)
+        # Generate noise to feed to the generator
+        z = np.random.uniform(-1., 1., size=[batch_size, noise_dim])
+
+        # Train
+        feed_dict = {disc_input: batch_x, gen_input: z}
+        _, _, gl, dl = sess.run([train_gen, train_disc, gen_loss, disc_loss],
+                                feed_dict=feed_dict)
+        if i % 1000 == 0 or i == 1:
+            print('Step %i: Generator Loss: %f, Discriminator Loss: %f' % (i, gl, dl))
+
+    # Generate images from noise, using the generator network.
+    f, a = plt.subplots(4, 10, figsize=(10, 4))
+    for i in range(10):
+        # Noise input.
+        z = np.random.uniform(-1., 1., size=[4, noise_dim])
+        g = sess.run([gen_sample], feed_dict={gen_input: z})
+        g = np.reshape(g, newshape=(4, 28, 28, 1))
+        for j in range(4):
+            # Generate image from noise. Extend to 3 channels for matplot figure.
+            img = np.reshape(np.repeat(g[j][:, :, np.newaxis], 3, axis=2),
+                             newshape=(28, 28, 3))
+            a[j][i].imshow(img)
+
+    f.show()
+    plt.draw()
+    plt.savefig('MNIST.png')
